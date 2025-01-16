e8eca59786e46183936a3a9acb867d32550d3674
KZS <KZS@a.a>
2025-01-16 09:25:41 +0100

    image slider added to home page

---
+++ web/scripts/home-page-slider.js
@@ -0,0 +1,18 @@
+const slides = document.querySelector(".slides");
+const slideCount = document.querySelectorAll(".slide").length;
+const slidesToShow = 3;
+const slideWidth = 100 / slidesToShow;
+let currentIndex = 0;
+
+function updateSlidePosition() {
+  slides.style.transform = `translateX(-${currentIndex * slideWidth}%)`;
+}
+
+function showNextSlide() {
+  currentIndex = currentIndex >= slideCount - slidesToShow ? 0 : currentIndex + 1;
+  updateSlidePosition();
+}
+
+setInterval(showNextSlide, 3000);
+
+updateSlidePosition();
